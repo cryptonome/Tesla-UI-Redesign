3fc0b30e41aed87704e4837e484dde5496837f78
skamalanathan3 <41276322+skamalanathan3@users.noreply.github.com>
2019-02-13 14:25:36 -0500

    Add files via upload

---
+++ Car Screen UI/BTH545-master/README.md
@@ -0,0 +1,15 @@
+"# BTH545" 
+
+### Steering Wheel/index.html has the steering wheel input
+
+## 
+
+## TODO
+
+* Main screen
+* Radio Sub-Menu
+* Music Setting Sub-Menu
+
+To view:
+Go into UI Home Page
+Open index.html
---
+++ Car Screen UI/BTH545-master/UI Home Page/js/main.js
@@ -0,0 +1,218 @@
+var radioSwitch = true;
+var isNavOpen = false;
+var song = 0;
+
+function park() {
+	$('#topleft').attr('src','./media/Park-S.svg');
+}
+
+function drive() {
+	$('#topleft').attr('src','./media/Drive-S.svg');
+}
+
+function reverse() {
+	$('#topleft').attr('src','./media/Reverse-S.svg');
+}
+
+function openNav() {
+if (document.getElementById("mySidenav").style.width == "0px") {
+  document.getElementById("mySidenav").style.width = "355px";
+  document.getElementById("menu-button").style.marginLeft = "355px";
+  isNavOpen = true;
+  $('#menu-button').attr('src','./media/menu/close-menu.svg');
+} else {
+	document.getElementById("mySidenav").style.width = "0px";
+	document.getElementById("menu-button").style.marginLeft = "0px";
+	$('#menu-button').attr('src','./media/menu/menu.svg');
+	isNavOpen = false;
+}
+}
+
+function toggleRadio() {
+	if (radioSwitch)
+	$('#radioSwitch').attr('src','./media/music/fm.svg');
+	else 
+	$('#radioSwitch').attr('src','./media/music/am.svg');
+	radioSwitch = !radioSwitch;
+}
+
+function upArrow() {
+  alert("wow")
+}
+//Change the song
+function switchSong(){
+	
+	song = (song == 0) ? 1 : 0;
+	
+	
+    var audio = document.getElementById("audio");
+	var source = document.getElementById("source");
+	if(isNavOpen == false) {
+		openNav();
+	}else{
+		setTimeout(function(){
+		openNav();
+		}, 2000);
+	}
+	if(song == 1) {
+		source.src = './media/musicmedia/Bacca.mp3';
+		//song = 0;
+	}else{
+		source.src = './media/musicmedia/Kappa.mp3';
+		//song = 1;
+	}
+	audio.load();
+	audio.play();
+}
+//Updates the slider value based on steering input
+function toggleVol_(val){
+	let btn = document.getElementById("volume");
+	let ls = document.getElementById("volume_ls");
+	
+	let lse = document.getElementById("myRange1");
+	let lse2 = document.getElementById("myRange2");
+	if(btn.style.border == '' || isNavOpen == false){
+		  
+		if(isNavOpen == false) {
+			openNav();
+			btn.style.border = '4px solid blue';
+		}else{
+			console.log("Did not open nav");
+		}
+		
+		
+		 
+	}else{
+		
+		console.log("Did not open nav due to border or nav");
+	}
+	
+	btn.style.border == '';
+	ls.click();
+	//Update the slider value
+	lse.value = lse2.value;
+	
+
+	if (val == 999) {
+		lse.value = 0;
+		lse2.value = 0;
+	}
+	
+}
+function goSetting(){
+	
+	let ls = document.getElementById("settings");
+	if(isNavOpen == false) {
+		openNav();
+	}
+	ls.click();
+	
+}
+function goSpotify(){
+	
+	let ls = document.getElementById("spotify");
+	if(isNavOpen == false) {
+		openNav();
+	}
+	ls.click();
+	
+}
+function goRadio(){
+	
+	let ls = document.getElementById("radio");
+	if(isNavOpen == false) {
+		openNav();
+	}
+	ls.click();
+	
+}
+function goAux(){
+	
+	let ls = document.getElementById("aux");
+	if(isNavOpen == false) {
+		openNav();
+	}
+	ls.click();
+	
+}
+//Toggles the menu for volume
+function toggleVol(){
+	
+	let btn = document.getElementById("volume");
+	let ls = document.getElementById("volume_ls");
+	  if(btn.style.border == ''){
+		  
+		 if(isNavOpen == false) {
+			 openNav();
+		 }
+		 btn.style.border = '4px solid blue';
+		
+		  
+	  }else{
+		  if(isNavOpen == true) {
+			 openNav();
+			}
+		  btn.style.border = '';
+		  btn.style.backgroundColor = '';
+	
+	  }
+	  ls.click();
+}
+//Toggles the menu for Bluetooth
+function toggleBlueTooth(){
+	let btn = document.getElementById("bluetooth");
+	let ls = document.getElementById("bluetooth_ls");
+	if(btn.style.border == ''){
+		 if(isNavOpen != true) {
+			 openNav();
+		 }
+		 btn.style.border = '4px solid blue';
+		
+		  
+	  }else{
+		    if(isNavOpen == true) {
+			 openNav();
+			}
+		  btn.style.border = '';
+		  btn.style.backgroundColor = '';
+	
+	  }
+	  ls.click();
+}
+//Toggles the menu for Wifi
+function toggleWifi(){
+	let btn = document.getElementById("wifi");
+	let ls = document.getElementById("wifi_ls");
+	if(btn.style.border == ''){
+		 if(isNavOpen != true) {
+			 openNav();
+		 }
+		 btn.style.border = '4px solid blue';
+		
+		  
+	  }else{
+		    if(isNavOpen == true) {
+			 openNav();
+			}
+		  btn.style.border = '';
+		  btn.style.backgroundColor = '';
+	
+	  }
+	  ls.click();
+}
+//Toggles the menu for Voice
+function toggleNotBar(){
+	
+	 let x = document.getElementById("togBar");
+	  if(x.style.display == "none"){
+		 
+		  x.style.display = "block";
+		  
+	  }else{
+		  
+		  x.style.display="none";
+	  }
+}
+document.getElementById("controls").contentWindow.document.getElementById("up").addEventListener("click, upArrow(), false");
+
+
